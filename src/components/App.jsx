512016b0feedaa000fd2b5848e26b964ff7f32b2
Johannes Demissie <demissie.johannes@gmail.com>
2022-08-26 23:54:34 +0200

    Refactor sidebar to be inside of FabricJS.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -38,8 +38,14 @@ function App() {
   }
 
   return (
-    <div id="row">
-      <div id="sidebar">
+    <div id="row" style={{ visibility: "visible" }}>
+      <div
+        id="sidebar"
+        style={{
+          visibility: uploaded ? "hidden" : "visible",
+          position: uploaded ? "absolute" : "relative",
+        }}
+      >
         <div>
           <h1 style={{ textAlign: "center" }}>Watermark</h1>
         </div>
@@ -49,94 +55,27 @@ function App() {
             <Form.Control type="file" onChange={(e) => GroupedFunction(e)} />
           </Form.Group>
         </div>
-
-        <div>
-          <Form.Control
-            type="text"
-            placeholder="Watermark text here..."
-            readOnly
-          />
-        </div>
-        <div style={{ marginBottom: "0" }}>
-          <Form.FloatingLabel>Select font:</Form.FloatingLabel>
-          <Form.Select>
-            <option value="arial">Arial</option>
-            <option value="helvetica" selected>
-              Helvetica
-            </option>
-            <option value="myriad pro">Myriad Pro</option>
-            <option value="delicious">Delicious</option>
-            <option value="verdana">Verdana</option>
-            <option value="georgia">Georgia</option>
-            <option value="courier">Courier</option>
-            <option value="comic sans ms">Comic Sans MS</option>
-            <option value="impact">Impact</option>
-            <option value="monaco">Monaco</option>
-            <option value="optima">Optima</option>
-            <option value="hoefler text">Hoefler Text</option>
-            <option value="plaster">Plaster</option>
-            <option value="engagement">Engagement</option>
-          </Form.Select>
-        </div>
-        <div style={{ display: "flex", margin: "0" }}>
-          <div style={{ margin: "auto" }}>
-            <InputGroup className="mb-3">
-              <InputGroup.Text>Bold</InputGroup.Text>
-              <InputGroup.Checkbox aria-label="Checkbox for following text input" />
-            </InputGroup>
-          </div>
-          <div></div>
-          <div style={{ margin: "auto" }}>
-            <InputGroup className="mb-3">
-              <InputGroup.Text>Italic</InputGroup.Text>
-              <InputGroup.Checkbox aria-label="Checkbox for following text input" />
-            </InputGroup>
-          </div>
-        </div>
-        <div style={{ marginTop: "0" }}>
-          <Form.Label htmlFor="exampleColorInput">Color picker:</Form.Label>
-          <Form.Control
-            type="color"
-            id="exampleColorInput"
-            defaultValue="#FFFFFF"
-            title="Choose your color"
-          />
-        </div>
-
-        <div>
-          <p>Transparency:</p>
-          <Form.Range />
-        </div>
-        <div style={{ display: "flex" }}>
-          <div style={{ margin: "auto" }}>
-            <Button onClick={refreshPage} variant="outline-primary">
-              Preview
-            </Button>{" "}
-          </div>
-          <div style={{ margin: "auto" }}>
-            <Button onClick={refreshPage} variant="primary">
-              Download
-            </Button>{" "}
-          </div>
-        </div>
-        <div style={{ margin: "30px auto 10px" }} className="d-grid gap-2">
-          <Button onClick={refreshPage} variant="danger">
-            Reset
-          </Button>{" "}
-        </div>
       </div>
-      <div id="workingSpace">
-        <h3>Uploaded Image: </h3>
+      <div
+        id="workingSpace"
+        style={{
+          margin: "auto",
+        }}
+      >
+        <h3
+          style={{
+            visibility: uploaded ? "hidden" : "visible",
+            position: uploaded ? "absolute" : "relative",
+          }}
+        >
+          Watermark Editor
+        </h3>
         <img id="my_picture" />
         {uploaded && (
           <div id="canvas-div">
             <div id="layer1">
               <FabricJS height={imageHeight} width={imageWidth} />
             </div>
-
-            <button id="download" onClick={() => Download()}>
-              Download
-            </button>
           </div>
         )}
       </div>
--- src/components/FabricJS.jsx
+++ src/components/FabricJS.jsx
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { fabric } from "fabric";
+import { InputToImage, Download, ImageDimensions } from "./Functions";
 import "./FabricJS.css";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import InputGroup from "react-bootstrap/InputGroup";
 
 function FabricJS({ height, width }) {
   // Define a state variable to store and access the fabric.Canvas object
@@ -83,49 +89,130 @@ function FabricJS({ height, width }) {
     canvas.remove(canvas.getActiveObject());
   }
 
+  // Reset the page to delete everything
+  function refreshPage() {
+    window.location.reload(false);
+  }
+
   return (
     <>
-      <h1>Test FabricJS</h1>
-      <div>
-        <button type="button" onClick={() => Draw()}>
-          Draw
-        </button>
-        <button type="button" onClick={() => DeleteObject()}>
-          Delete
-        </button>
-        <button type="button" onClick={() => AddImage()}>
-          Add Image
-        </button>
-        <button
-          type="button"
-          onClick={() => canvas.discardActiveObject().renderAll()}
-        >
-          Deactivate
-        </button>
-        <input
-          type="range"
-          id="opacity"
-          defaultValue="-50"
-          min="-100"
-          max="-1"
-          onChange={(event) => Slider(event)}
-          onClick={(event) => Slider(event)}
-        />
-        <h3>Transparency: {transparency}%</h3>
-        <h3>The opacity range is {opacity}</h3>
+      <div id="rowFabric">
+        <div id="sidebarFabric">
+          <h1>Test FabricJS</h1>
+          <div style={{ margin: "30px auto 10px" }} className="d-grid gap-2">
+            <Button onClick={refreshPage} variant="danger">
+              Reset
+            </Button>{" "}
+          </div>
+          <div>
+            <Form.Control
+              type="text"
+              placeholder="Watermark text here..."
+              readOnly
+            />
+          </div>
+          <div style={{ marginBottom: "0" }}>
+            <Form.FloatingLabel>Select font:</Form.FloatingLabel>
+            <Form.Select>
+              <option value="arial">Arial</option>
+              <option value="helvetica" selected>
+                Helvetica
+              </option>
+              <option value="myriad pro">Myriad Pro</option>
+              <option value="delicious">Delicious</option>
+              <option value="verdana">Verdana</option>
+              <option value="georgia">Georgia</option>
+              <option value="courier">Courier</option>
+              <option value="comic sans ms">Comic Sans MS</option>
+              <option value="impact">Impact</option>
+              <option value="monaco">Monaco</option>
+              <option value="optima">Optima</option>
+              <option value="hoefler text">Hoefler Text</option>
+              <option value="plaster">Plaster</option>
+              <option value="engagement">Engagement</option>
+            </Form.Select>
+          </div>
+          <div style={{ display: "flex", margin: "0" }}>
+            <div style={{ margin: "auto" }}>
+              <InputGroup className="mb-3">
+                <InputGroup.Text>Bold</InputGroup.Text>
+                <InputGroup.Checkbox aria-label="Checkbox for following text input" />
+              </InputGroup>
+            </div>
+            <div></div>
+            <div style={{ margin: "auto" }}>
+              <InputGroup className="mb-3">
+                <InputGroup.Text>Italic</InputGroup.Text>
+                <InputGroup.Checkbox aria-label="Checkbox for following text input" />
+              </InputGroup>
+            </div>
+          </div>
+          <div style={{ marginTop: "0" }}>
+            <Form.Label htmlFor="exampleColorInput">Color picker:</Form.Label>
+            <Form.Control
+              type="color"
+              id="exampleColorInput"
+              defaultValue="#FFFFFF"
+              title="Choose your color"
+            />
+          </div>
+
+          <div>
+            <p>Transparency:</p>
+            <Form.Range
+              defaultValue="-50"
+              min="-100"
+              max="-1"
+              onChange={(event) => Slider(event)}
+              onClick={(event) => Slider(event)}
+            />
+          </div>
+          <div style={{ display: "flex" }}>
+            <div style={{ margin: "auto" }}>
+              <Button onClick={null} variant="outline-primary">
+                Preview
+              </Button>{" "}
+            </div>
+            <div style={{ margin: "auto" }}>
+              <Button onClick={() => Download()} variant="primary">
+                Download
+              </Button>{" "}
+            </div>
+          </div>
+        </div>
+
+        <div id="workingSpace" style={{ margin: "auto" }}>
+          <button type="button" onClick={() => Draw()}>
+            Draw
+          </button>
+          <button type="button" onClick={() => DeleteObject()}>
+            Delete
+          </button>
+          <button type="button" onClick={() => AddImage()}>
+            Add Image
+          </button>
+          <button
+            type="button"
+            onClick={() => canvas.discardActiveObject().renderAll()}
+          >
+            Deactivate
+          </button>
+          <h3>Transparency: {transparency}%</h3>
+          <div>
+            <canvas
+              id="canvas"
+              width={height}
+              height={width}
+              style={{
+                border: "1px solid #000000",
+                // Set the aspect ratio of the picture to prevent stretching
+                maxWidth: `${500 * (width / height)}px`,
+                maxHeight: "500px",
+              }}
+            ></canvas>
+          </div>
+        </div>
       </div>
-      <br />
-      <canvas
-        id="canvas"
-        width={height}
-        height={width}
-        style={{
-          border: "1px solid #000000",
-          // Set the aspect ratio of the picture to prevent stretching
-          maxWidth: `${500 * (width / height)}px`,
-          maxHeight: "500px",
-        }}
-      ></canvas>
     </>
   );
 }
